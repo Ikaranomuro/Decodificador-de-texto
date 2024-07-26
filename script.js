75e5ae4a2ee5a52d87b37f7fb1bff1134eb0dafb
Iago Oliveira Fonseca <iagooliveirafonseca83@gmail.com>
2024-07-26 17:02:49 -0300

    Finaliznado o projeto

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-function verificarTamanhoDaTela(){
-    let body = document.getElementsByTagName('body')[0]
+/* RESPONSIVIDADE PARA O TEXTAREA*/
+function verificarTamanhoDaTela() {
+    let body = document.getElementsByTagName('body')[0];
     let textAreaValue = document.getElementsByClassName("section__inputText")[0];
 
-
-    if(body.offsetWidth <= 765){
-        console.log("tela de celular")
-        textAreaValue.setAttribute('cols', 20)
-    } else{
-        console.log("tela de outro navegador")
+    if (body.offsetWidth <= 765) {
+        textAreaValue.setAttribute('cols', 20);
+        textAreaValue.setAttribute('maxlength', 100);
+        
     }
 }
 
-verificarTamanhoDaTela()
+verificarTamanhoDaTela();
 
 window.addEventListener('resize', verificarTamanhoDaTela);
 
+/*CRIANDO A LÓGICA DE CRIPTOGRAFAR E DESCRIPTOGRAFAR*/
+
 
 function criptografar() {
-    
-    let textAreaValue = document.getElementsByClassName("section__inputText")[0];
+
+    let textArea = document.getElementsByClassName("section__inputText")[0];
+    let textAreaValue = textArea.value
     let titleDissapear = document.getElementsByTagName("h2")[0];
     let paragrafhDissapear = document.getElementsByTagName("p")[0];
     let imagePersonDisspear = document.getElementsByClassName("aside__Person_image")[0];
     let textEncrypted = document.getElementsByClassName('text_encrypted')[0];
     let buttonCopy = document.getElementsByClassName("button_Copy")[0];
- 
 
-
-
-    if (textAreaValue.value == "") {
-        textAreaValue.placeholder = 'Por favor, coloque uma mensagem!'
+    if (textAreaValue == "") {
+        textArea.value = 'Por favor, coloque uma mensagem!';
     } else {
-        paragrafhDissapear.style.display = "none"
-        titleDissapear.style.display = "none"
-        imagePersonDisspear.style.display = "none"
-        textEncrypted.textContent = `${textAreaValue.value}`
-        buttonCopy.classList.toggle = "buttonAppear"
-      
-
-        textoConvertido = textAreaValue.value.replace(/e/g, (e) => {
-            e = "enter"
-            return e
-        });
-        // textoConvertido = textAreaValue.value.replace(/i/g , (i)=>{
-        //     i = "imes"
-        //     return i
-        // });
-
-        console.log(textoConvertido)
-
-
+        paragrafhDissapear.style.display = "none";
+        titleDissapear.style.display = "none";
+        imagePersonDisspear.style.display = "none";
+        buttonCopy.style.display = "block";
+        textoCriptografado = textAreaValue.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+        textEncrypted.textContent = `${textoCriptografado}`;
+        textArea.value = "" 
     }
 
 }
 
 
+function descriptografar() {
+    let textArea = document.getElementsByClassName("section__inputText")[0];
+    let paragrafhDissapear = document.getElementsByTagName("p")[0];
+    let textEncrypted = document.getElementsByClassName('text_encrypted')[0];
+ 
+    if(textEncrypted.textContent == ""){
+        console.log("Está vázio");
+        paragrafhDissapear.textContent = "Por favor, criptografe um texto, para depois descriptografar";
+    } else{
+        let textEncryptedValue = textEncrypted.textContent;
+        textoDescriptografado = textEncryptedValue.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+        console.log(textoDescriptografado);
+        textArea.value = textoDescriptografado;
+    }
+}
 
-// resultado = textAreaValue.value.split("")
-// console.log(resultado)
+function copyText(){
+    let textEncrypted = document.getElementsByClassName('text_encrypted')[0].textContent;
+    navigator.clipboard.writeText(textEncrypted)
+}   
